7ed13814b32c11cab2b5c95f6d7792ec434546b4
PauloAugustojv <paulinho200111@oulook.com>
2023-11-28 07:13:11 -0300

    main

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
+const calcularMargem = document.getElementById('calcularMargem')
 const calcular17 = document.getElementById('calcular17')
 const calcular25 = document.getElementById('calcular25')
-const calcularMargem = document.getElementById('calcularMargem')
+const calcularDesconto = document.getElementById('calcularDesconto')
+const resultadoDescontoSpan = document.getElementById('resultadoDesconto')
+
+calcularMargem.addEventListener('click', function () {
+  calcularPrecoVenda(
+    parseFloat(document.getElementById('margemDesejada').value) / 100
+  )
+})
 
 calcular17.addEventListener('click', function () {
-  calcularPrecoVenda(0.17) // Margem de 17%
+  calcularPrecoVenda(0.17)
 })
 
 calcular25.addEventListener('click', function () {
-  calcularPrecoVenda(0.25) // Margem de 25%
+  calcularPrecoVenda(0.25)
 })
 
-calcularMargem.addEventListener('click', function () {
-  const margemDesejada = parseFloat(
-    document.getElementById('margemDesejada').value
+calcularDesconto.addEventListener('click', function () {
+  const precoVenda = parseFloat(
+    document.getElementById('precoVendaDesconto').value
+  )
+  const precoDesconto = parseFloat(
+    document.getElementById('precoDesconto').value
   )
 
-  if (isNaN(margemDesejada)) {
-    alert('Por favor, insira uma margem desejada válida.')
+  if (isNaN(precoVenda) || isNaN(precoDesconto)) {
+    alert(
+      'Por favor, insira valores válidos para o preço de venda e o desconto.'
+    )
     return
   }
 
-  calcularPrecoVenda(margemDesejada / 100) // Converte a margem desejada em decimal
+  const resultadoDesconto = ((precoVenda / precoDesconto - 1) * 100).toFixed(9)
+  resultadoDescontoSpan.textContent = `Resultado do Desconto: ${resultadoDesconto}%`
 })
 
 function calcularPrecoVenda(rentabilidade) {
@@ -34,6 +48,7 @@ function calcularPrecoVenda(rentabilidade) {
   }
 
   const precoVenda = precoCusto / (1 - rentabilidade)
-  document.getElementById('precoVenda').textContent =
-    'R$ ' + precoVenda.toFixed(2)
+  document.getElementById('precoVenda').textContent = `R$ ${precoVenda.toFixed(
+    2
+  )}`
 }
